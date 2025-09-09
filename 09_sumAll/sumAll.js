6f21c3a742c119793980e7fd4b8ec4667a11d9a8
teomac <liquidinobusiness@gmail.com>
2025-09-09 23:55:16 +0200

    Done 09 javascript exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+  if (a >= 0 && b >= 0) {
+    var sum = 0;
+    for (var i = a; i <= b; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
