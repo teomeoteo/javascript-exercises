aa1a87965bbc25791483801fbf56e963056ac856
teomac <liquidinobusiness@gmail.com>
2025-09-09 23:41:06 +0200

    Complete exercise 08-removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  let newArray  = array.filter(e => !args.includes(e));
+  return newArray;
 };
 
 // Do not edit below this line
