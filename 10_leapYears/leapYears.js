fa24388115ffd1196d5c100f4f52658e3a863bbd
teomac <liquidinobusiness@gmail.com>
2025-09-10 00:06:31 +0200

    Complete exercise 10 and stop bcus 11 is too easy and its late

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  if (year % 4 === 0 && year % 4 === 0 && !(year % 100 === 0)) {
+    return true;
+  }
+
+  else {
+    return false;
+  }
+
 
 };
 
