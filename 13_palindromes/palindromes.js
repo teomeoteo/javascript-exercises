5928c3878d111e73300b50813cbb1757a6d6422e
teomac <liquidinobusiness@gmail.com>
2025-09-15 23:26:19 +0200

    Done palindrome exercise and learned about regex

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let clearStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  let splitStr = clearStr.split('');
+  let reversedString = "";
+  for (let i = splitStr.length - 1; i >= 0; i--) {
+    reversedString += splitStr[i];
+  }
+  if (reversedString === clearStr) {
+    return true;
+  }
 
+  else {
+    return false;
+  }
 };
 
 // Do not edit below this line
